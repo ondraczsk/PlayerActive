76c62732ed00e0f644d5dbdcfdb6beb2e7189268
ondraczsk <ondrakraftak@gmail.com>
2016-12-12 16:55:13 +0100

    Update PlayerActive.php

--- src/ondraczsk/PlayerActive.php
+++ src/ondraczsk/PlayerActive.php
@@ -1,3 +1,77 @@
 <?php
 
-namespace Ondra
+/* PlayerActive plugin
+ * by ondraczsk 2016
+ */
+
+namespace ondraczsk\PlayerActive;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\utils\TextFormat;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\utils\Config;
+
+class PlayerActive extends PluginBase implements Listener{
+  
+  /**
+     * Translate Minecraft colors
+     * 
+     * @param char $symbol Color symbol
+     * @param string $message The message to be translated
+     * 
+     * @return string The translated message
+     */
+    public function translateColors($symbol, $message){
+    
+    	$message = str_replace($symbol."0", TextFormat::BLACK, $message);
+    	$message = str_replace($symbol."1", TextFormat::DARK_BLUE, $message);
+    	$message = str_replace($symbol."2", TextFormat::DARK_GREEN, $message);
+    	$message = str_replace($symbol."3", TextFormat::DARK_AQUA, $message);
+    	$message = str_replace($symbol."4", TextFormat::DARK_RED, $message);
+    	$message = str_replace($symbol."5", TextFormat::DARK_PURPLE, $message);
+    	$message = str_replace($symbol."6", TextFormat::GOLD, $message);
+    	$message = str_replace($symbol."7", TextFormat::GRAY, $message);
+    	$message = str_replace($symbol."8", TextFormat::DARK_GRAY, $message);
+    	$message = str_replace($symbol."9", TextFormat::BLUE, $message);
+    	$message = str_replace($symbol."a", TextFormat::GREEN, $message);
+    	$message = str_replace($symbol."b", TextFormat::AQUA, $message);
+    	$message = str_replace($symbol."c", TextFormat::RED, $message);
+    	$message = str_replace($symbol."d", TextFormat::LIGHT_PURPLE, $message);
+    	$message = str_replace($symbol."e", TextFormat::YELLOW, $message);
+    	$message = str_replace($symbol."f", TextFormat::WHITE, $message);
+    
+    	$message = str_replace($symbol."k", TextFormat::OBFUSCATED, $message);
+    	$message = str_replace($symbol."l", TextFormat::BOLD, $message);
+    	$message = str_replace($symbol."m", TextFormat::STRIKETHROUGH, $message);
+    	$message = str_replace($symbol."n", TextFormat::UNDERLINE, $message);
+    	$message = str_replace($symbol."o", TextFormat::ITALIC, $message);
+    	$message = str_replace($symbol."r", TextFormat::RESET, $message);
+    
+    	return $message;
+    }
+  
+  public function onEnable(){
+    $this->getServer()->getPluginManager()->registerEvents($this,$this);
+    $this->getLogger()->info("Plugin se zapnul");
+    @mkdir($this->getDataFolder());
+    $this->config = new Config ($this->getDataFolder() . "config.yml" , Config::YAML, array());
+    $this->config->set("join-message");
+    $this->config->set("quit-message");
+  }
+  public function onJoin(PlayerJoinEvent $event){
+    $player = $event->getPlayer();
+    $name = $player->getName();
+    $cfg = $this->plugin->getConfig()->getAll();
+    $this->getServer()->broadcastMessage($this->plugin->translateColors("&", $cfg["join-message"]));
+  }
+  public function onQuit(PlayerQuitEvent $event){
+        $player = $event->getPlayer();
+    $name = $player->getName();
+    $cfg = $this->plugin->getConfig()->getAll();
+    $this->getServer()->broadcastMessage($this->plugin->translateColors("&", $cfg["quit-message"]));
+  }
+    
+
+    
